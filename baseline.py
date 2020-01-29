3cfa9fdf945ed9ab9878fd69bb9582827c2c078d
hanuszczak <hanuszczak@gmail.com>
2020-01-29 17:41:54 +0100

    done

--- baseline.py
+++ baseline.py
@@ -1,5 +1,3 @@
-import string
-
 import numpy as np
 import pandas as pd
 import copy
@@ -71,6 +69,13 @@ def check_if_min_3_gifts_in_every_bag(individual):
     return True
 
 
+def check_if_every_gift_in_one_or_none_bag(individual):
+    used_gifts = np.sum(individual, axis=0)
+    if np.any(used_gifts > 1):
+        return False
+    return True
+
+
 def chcek_if_all_bags_not_overloaded(individual, weight):
     for i in range(0, 1000):
         w = bag_sum(individual, weight, i)
@@ -79,99 +84,20 @@ def chcek_if_all_bags_not_overloaded(individual, weight):
     return True
 
 
-def cross_over(population, cross_over_probability, list_of_individuals_to_cross_over, initial_population_size,
-               elite_count, weights, max_tries):
+def only_mutate(population, weight_dict, mutation_probability, max_tries, list_of_selected):
     new_population = list()
-    list_of_split_rate = list(range(0, 7166))
-    random.shuffle(list_of_split_rate)
-    random.shuffle(list_of_split_rate)
-    random.shuffle(list_of_split_rate)
-    split_rate = list_of_split_rate.pop()
 
-    number_of_ind_to_cross = (int(len(list_of_individuals_to_cross_over)*cross_over_probability/2))*2
-    number_of_kids_from_crossing = int(number_of_ind_to_cross/2)
-    list_to_cross_over = list_of_individuals_to_cross_over[:number_of_ind_to_cross]
-    list_to_pass_over = list_of_individuals_to_cross_over[number_of_ind_to_cross:]
-    list_to_pass_over = list_to_pass_over[:max(initial_population_size - number_of_kids_from_crossing - elite_count, 0)]
-    while len(list_to_pass_over) > 0:
-        pop = list_to_pass_over.pop()
-        #print(f"len population: {len(population)} | last list_to_pass_over: {pop}")
-        new_population.append(population[pop])
-
-    first_parent_idx = list_to_cross_over.pop()
-    second_parent_idx = list_to_cross_over.pop()
-    while len(list_to_cross_over) > 1:
-        #print(f"first_parent_idx: {first_parent_idx} | second_parent_idx:{second_parent_idx}")
-        ind_1 = population[first_parent_idx]
-        ind_2 = population[second_parent_idx]
-        ind_1_back = ind_1[:, split_rate:]
-        ind_1_front = ind_1[:, :split_rate]
-        ind_2_back = ind_2[:, split_rate:]
-        ind_2_front = ind_2[:, :split_rate]
-        new_1 = np.concatenate((ind_2_front, ind_1_back), axis=1)
-        new_2 = np.concatenate((ind_1_front, ind_2_back), axis=1)
-        tries = max_tries
-        new_1_ok = False
-        new_2_ok = False
-        if check_if_min_3_gifts_in_every_bag(new_1) and chcek_if_all_bags_not_overloaded(new_1, weights):
-            print("new_1_ok!")
-            new_1_ok = True
-        if check_if_min_3_gifts_in_every_bag(new_2) and chcek_if_all_bags_not_overloaded(new_2, weights):
-            print("new_2_ok!")
-            new_2_ok = True
-        while tries > 0 and len(list_of_split_rate) > 0 and not new_1_ok and not new_2_ok:
-            split_rate = list_of_split_rate.pop()
-            ind_1_back = ind_1[:, split_rate:]
-            ind_1_front = ind_1[:, :split_rate]
-            ind_2_back = ind_2[:, split_rate:]
-            ind_2_front = ind_2[:, :split_rate]
-            new_1 = np.concatenate((ind_2_front, ind_1_back), axis=1)
-            new_2 = np.concatenate((ind_1_front, ind_2_back), axis=1)
-            if check_if_min_3_gifts_in_every_bag(new_1) and chcek_if_all_bags_not_overloaded(new_1, weights):
-                print("new_1_ok!")
-                new_1_ok = True
-            if check_if_min_3_gifts_in_every_bag(new_2) and chcek_if_all_bags_not_overloaded(new_2, weights):
-                print("new_2_ok!")
-                new_2_ok = True
-            tries = tries - 1
-        if new_1_ok and new_2_ok:
-            print("both new ok")
-            if count_rate(new_1, weights) > count_rate(new_2, weights):
-                new_population.append(new_1)
-            else:
-                new_population.append(new_2)
-        elif new_1_ok:
-            print("new 1 ok")
-            new_population.append(new_1)
-        elif new_2_ok:
-            print("new 2 ok")
-            new_population.append(new_2)
-        else:
-            if generate_random_number() < 0.5:
-                print("parent 1 appended")
-                new_population.append(population[first_parent_idx])
-            else:
-                print("parent 2 appended")
-                new_population.append(population[second_parent_idx])
-        print(f"crossing - tries left: {tries}")
-        first_parent_idx = list_to_cross_over.pop()
-        second_parent_idx = list_to_cross_over.pop()
-    return new_population
-
-
-def mutate(population, weight_dict, mutation_probability, max_tries):
-    list_of_individuals_to_mutate = list(range(0, len(population)))
+    list_of_individuals = list(range(0, len(list_of_selected)))
     number_of_ind_to_mutate = int(len(population)*mutation_probability)
+    list_to_mutate = list_of_individuals[:number_of_ind_to_mutate]
+    list_to_pass = list_of_individuals[number_of_ind_to_mutate:]
 
-    random.shuffle(list_of_individuals_to_mutate)
-    random.shuffle(list_of_individuals_to_mutate)
-    random.shuffle(list_of_individuals_to_mutate)
+    while len(list_to_pass) > 0:
+        new_population.append(copy.deepcopy(population[list_of_selected[list_to_pass.pop()]]))
 
-    list_of_individuals_to_mutate = list_of_individuals_to_mutate[:number_of_ind_to_mutate]
-    to_mutate_idx = list_of_individuals_to_mutate.pop()
-
-    while len(list_of_individuals_to_mutate) > 0:
-        individual = population[to_mutate_idx]
+    while len(list_to_mutate) > 0:
+        to_mutate_idx = list_to_mutate.pop()
+        individual = copy.deepcopy(population[list_of_selected[to_mutate_idx]])
         row_to_mutate = random.randint(0, 999)
         available_gifts = list()
         used_gifts = np.sum(individual, axis=0)
@@ -183,46 +109,14 @@ def mutate(population, weight_dict, mutation_probability, max_tries):
         gift_to_add = available_gifts.pop()
 
         try_counter = max_tries
-
         while bag_sum(individual, weight_dict, row_to_mutate) + weight_dict[gift_to_add] > 50.0 \
                 and try_counter > 0 and len(available_gifts) > 0:
             gift_to_add = available_gifts.pop()
             try_counter = try_counter - 1
         if try_counter != 0 and available_gifts != 0:
             individual[row_to_mutate][gift_to_add] = 1.0
-       # population[to_mutate_idx] = individual
-        to_mutate_idx = list_of_individuals_to_mutate.pop()
-
-
-def mutate_with_best_approx_potential(population, weight_dict, mutation_probability, max_tries):
-    number_of_ind_to_mutate = int(len(population) * mutation_probability)
-
-    population_potentials = list()
-    for ind in population:
-        w = count_rate(ind, weight_dict)
-        population_potentials.append(w)
-    res = sorted(range(len(population_potentials)),
-                 key=lambda sub: population_potentials[sub])[:number_of_ind_to_mutate]
-    for r in range(len(res)):
-        individual = population[r]
-        row_to_mutate = random.randint(0, 999)
-        available_gifts = list()
-        used_gifts = np.sum(individual, axis=0)
-        for i, val in enumerate(used_gifts):
-            if not val:
-                available_gifts.append(i)
-
-        random.shuffle(available_gifts)
-        gift_to_add = available_gifts.pop()
-
-        try_counter = max_tries
-
-        while bag_sum(individual, weight_dict, row_to_mutate) + weight_dict[gift_to_add] > 50.0 \
-                and try_counter > 0 and len(available_gifts) > 0:
-            gift_to_add = available_gifts.pop()
-            try_counter = try_counter - 1
-        if try_counter != 0 and available_gifts != 0:
-            individual[row_to_mutate][gift_to_add] = 1.0
+        new_population.append(individual)
+    return new_population
 
 
 def bag_sum(individual, weight_dict, bag_no):
@@ -231,7 +125,7 @@ def bag_sum(individual, weight_dict, bag_no):
 
 def count_rate(individual, weight_dict):
     used_gifts = np.sum(individual, axis=0)
-    return np.sum(weight_dict*used_gifts);
+    return np.sum(weight_dict*used_gifts)
 
 
 def my_generate_initial_population(weight_dict, initial_population_size):
@@ -253,20 +147,15 @@ def my_generate_initial_population(weight_dict, initial_population_size):
             gift_1 = presents_to_start[-1]
             gift_2 = presents_to_start[-2]
             gift_3 = presents_to_start[-3]
-            gift_4 = presents_to_start[-4]
-            while weight_dict[gift_1] + weight_dict[gift_2] + \
-                    weight_dict[gift_3] + weight_dict[gift_4] > 50 and tries > 0:
+            while weight_dict[gift_1] + weight_dict[gift_2] + weight_dict[gift_3] > 50 and tries > 0:
                 random.shuffle(presents_to_start)
                 gift_1 = presents_to_start[-1]
                 gift_2 = presents_to_start[-2]
                 gift_3 = presents_to_start[-3]
-                gift_4 = presents_to_start[-4]
                 tries = tries - 1
             individual[bag][gift_1] = 1
             individual[bag][gift_2] = 1
             individual[bag][gift_3] = 1
-            individual[bag][gift_4] = 1
-            presents_to_start.pop()
             presents_to_start.pop()
             presents_to_start.pop()
             presents_to_start.pop()
@@ -274,7 +163,7 @@ def my_generate_initial_population(weight_dict, initial_population_size):
     return list_of_initial_population
 
 
-def selection(population, weight, elite_count, initial_population_size):
+def selection_for_mutation(population, weight, elite_count, initial_population_size):
     population_weights = list()
     random_list = list(range(0, 1000))
     random.shuffle(random_list)
@@ -284,8 +173,8 @@ def selection(population, weight, elite_count, initial_population_size):
         w = count_rate(ind, weight)
         population_weights.append(w)
     population_sum = np.sum(population_weights)
-    res = sorted(range(len(population_weights)),reverse=True, key=lambda sub: population_weights[sub])
-    proportions_indication_list = list(range(0,1000))
+    res = sorted(range(len(population_weights)), reverse=True, key=lambda sub: population_weights[sub])
+    proportions_indication_list = list(range(0, 1000))
     end_idx = 0
     proportion_sum = 0
     for j in res:
@@ -319,7 +208,19 @@ def print_bags_to_file(individual, names, weight, filename):
                 print(f"\t\t{names[i]}, {weight[i]}", file=filename)
 
 
-def algorithm(initial_population_size, elite_count, mutation_probability, cross_over_probability, max_tries, plot_name):
+def get_elitists(population, elite_count, weight):
+    elitists = list()
+    if elite_count == 0:
+        return elitists
+    sort = sorted(population, key=lambda g: count_rate(g, weight))
+    for i in range(elite_count):
+        elitists.append(sort[-(i+1)])
+
+    return elitists
+
+
+def algorithm(initial_population_size, elite_count, mutation_probability, max_tries, plot_name,
+              elite_reduction=False, reduction_iteration=1000, reduction_size=1):
     name, weight = get_gift_dicts()
     start = time.process_time()
     start1 = time.time()
@@ -329,21 +230,28 @@ def algorithm(initial_population_size, elite_count, mutation_probability, cross_
     list_of_results = list()
     limit = con.max_bag_capacity * con.max_number_of_bags
     max_weight = 0
-    sample = open(f"random-{plot_name}.txt", 'w')
-    print(f"Start of algorithm with max {con.max_iterations} iteration, "
+    sample = open(f"{plot_name}.txt", 'w')
+    print(f"Start of mutation algorithm with max {con.max_iterations} iteration, "
           f"initial population size: {initial_population_size}")
-    print(f"Mutation probability: {mutation_probability}, Crossing-over probability: {cross_over_probability}")
+    print(f"Mutation probability: {mutation_probability}")
     print(f"Start of algorithm with max {con.max_iterations} iteration, "
           f"initial population size: {initial_population_size}", file=sample)
-    print(f"Mutation probability: {mutation_probability}, Crossing-over probability: {cross_over_probability}",
+    print(f"Mutation probability: {mutation_probability}",
           file=sample)
     while max_weight < limit and iteration < con.max_iterations and const_counter < con.max_iteration_with_same_max:
-        list_to_cross_over = selection(population, weight, elite_count, initial_population_size)
-        population = cross_over(population, cross_over_probability, list_to_cross_over, initial_population_size,
-                                elite_count, weight, max_tries)
-
-        mutate(population, weight, mutation_probability, max_tries)
+        for c in range(len(population)):
+            check_if_every_gift_in_one_or_none_bag(population[0])
+        if elite_reduction:
+            if iteration % reduction_iteration == 0 and iteration != 0:
+                if reduction_size > 0:
+                    elite_count = max(elite_count - reduction_size, 0)
+                else:
+                    elite_count = min(elite_count - reduction_size, len(population)-2)
+        elite = get_elitists(population, elite_count, weight)
+        selected_list = selection_for_mutation(population, weight, elite_count, initial_population_size)
+        population = only_mutate(population, weight, mutation_probability, max_tries, selected_list)
         list_of_rates = list()
+        population.extend(elite)
         for g in population:
             w = count_rate(g, weight)
             list_of_rates.append(w)
@@ -353,7 +261,7 @@ def algorithm(initial_population_size, elite_count, mutation_probability, cross_
             const_counter = 0
         else:
             const_counter = const_counter + 1
-        if iteration % 5 == 0:
+        if iteration % 250 == 0:
             print(f"Iteration: {iteration}, Maximum weight: {max(list_of_rates)}")
         iteration = iteration + 1
     end = time.process_time()
@@ -366,30 +274,34 @@ def algorithm(initial_population_size, elite_count, mutation_probability, cross_
     for i in range(len(list_of_results)):
         print(f"Iteration: {i}, best in iteration: {list_of_results[i]}", file=sample)
     print(f"PRESENTS IN BAGS", file=sample)
-    print_bags_to_file(population[0], name, weight, sample)
-    create_plot(list_of_results, f"random-{plot_name}")
+    best = get_elitists(population, 1, weight)
+    if not check_if_every_gift_in_one_or_none_bag(best[0]):
+        print(f"WARNING! Best individual has at least one present which is in more than one bag")
+    else:
+        print_bags_to_file(best[0], name, weight, sample)
+    create_plot(list_of_results, f"{plot_name}")
     sample.close()
 
 
 def main():
-    # for k in range(10):
-    #     for l in range(len(con.mutation_probability_list)):
-    #         for m in range(len(con.cross_over_probability_list)):
-    #             for o in range(len(con.initial_population_size)):
-    #                 for p in range(len(con.max_tries)):
-    #                     timestamp1 = time.time()
-    #                     plot_name = f"Comparision-Run{timestamp1}-p{con.initial_population_size[o]}-" \
-    #                                 f"i{con.max_iterations}-m{con.mutation_probability_list[l]}-" \
-    #                                 f"c{con.cross_over_probability_list[m]}-t{con.max_tries[p]}"
-    #                     algorithm_with_potential(0.7, 0.7, plot_name)
-    #                     timestamp2 = time.time()
-    #                     plot_name = f"Comparision-Run{timestamp2}-p{con.initial_population_size[o]}-" \
-    #                                 f"i{con.max_iterations}-m{con.mutation_probability_list[l]}-" \
-    #                                 f"c{con.cross_over_probability_list[m]}-t{con.max_tries[p]}"
-    #                     algorithm(0.7, 0.7, plot_name)
-    timestamp1 = time.time()
-    plot_name = f"Comparision-Run{timestamp1}"
-    algorithm(20, 0, 0.7, 0.7, 20, plot_name)
+    for k in range(10):
+        timestamp1 = time.time()
+        plot_name = f"elite_0-{timestamp1}"
+        algorithm(con.initial_population_size, 0, con.mutation_probability, con.max_tries, plot_name)
+        timestamp2 = time.time()
+        plot_name = f"elite_1-{timestamp2}"
+        algorithm(con.initial_population_size, 1, con.mutation_probability, con.max_tries, plot_name)
+        timestamp3 = time.time()
+        plot_name = f"elite_5-{timestamp3}"
+        algorithm(con.initial_population_size, 5, con.mutation_probability, con.max_tries, plot_name)
+        timestamp4 = time.time()
+        plot_name = f"elite_decrease{timestamp4}"
+        algorithm(con.initial_population_size, 5, con.mutation_probability, con.max_tries, plot_name,
+                  True, 1000, 1)
+        timestamp5 = time.time()
+        plot_name = f"elite_growth{timestamp5}"
+        algorithm(con.initial_population_size, 0, con.mutation_probability, con.max_tries, plot_name,
+                  True, 1000, -1)
 
 
 if __name__ == "__main__":
--- constants.py
+++ constants.py
@@ -1,8 +1,7 @@
-cross_over_probability_list = (0.7, 0.5, 0.3)
-mutation_probability_list = (0.7, 0.5, 0.3)
-initial_population_size = (200, 100, 50)
-max_iterations = 25
+mutation_probability = 0.8
+initial_population_size = 20
+max_iterations = 15000
 max_number_of_bags = 1000
 max_bag_capacity = 50
-max_tries = (100, 50, 20)
-max_iteration_with_same_max = 2000
+max_tries = 50
+max_iteration_with_same_max = 1000
